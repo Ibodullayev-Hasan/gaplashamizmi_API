210bb0cb43446a9f845c77da761c8973196e9e1c
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-02-26 08:30:02 +0500

    'https://gaplashamiz.netlify.app' domeniga ruxsat berildi

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -3,7 +3,7 @@ import * as cookieParser from "cookie-parser";
 
 export function corsConfig(app: INestApplication) {
 	app.enableCors({
-		origin: "http://localhost:5173",
+		origin: ["http://localhost:5173", "https://gaplashamiz.netlify.app"],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
 	});
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -11,7 +11,7 @@ export class UsersController {
 
   // register
   @Post('new')
-  @UseGuards(EmailGuard)
+  // @UseGuards(EmailGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
