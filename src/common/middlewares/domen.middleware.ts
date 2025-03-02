1483db1d0338e857cd79a941b84f4f622f6dd34c
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-02 17:31:32 +0500

    test

--- src/common/middlewares/domen.middleware.ts
+++ src/common/middlewares/domen.middleware.ts
@@ -4,12 +4,15 @@ import { Request, Response } from "express";
 export class DomenMiddleware implements NestMiddleware {
 
 	private readonly allowedDomains = ['localhost', 'gaplashamizmi-api.up.railway.app']
+	private readonly allowedIp = ['::ffff:100.64.0.2']
 	use(req: Request, res: Response, next: (error?: Error | any) => void) {
 		const host = req.headers.host?.split(':')[0];
-		console.log(host);		
-		console.log(req.ip);		
-		
-		if (!this.allowedDomains.includes(host)) {
+		const ipAdress = req.ip
+
+		console.log(host);
+		console.log(req.ip);
+
+		if (!this.allowedDomains.includes(host) && this.allowedIp.includes(ipAdress)) {
 			throw new ForbiddenException('Access denied');
 		}
 
