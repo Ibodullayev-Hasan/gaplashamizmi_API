59c7d4ccaaa46d4e2bccb11522d7c65e1dcad412
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-02 17:44:37 +0500

    test

--- src/common/middlewares/domen.middleware.ts
+++ src/common/middlewares/domen.middleware.ts
@@ -2,17 +2,21 @@ import { ForbiddenException, NestMiddleware } from "@nestjs/common";
 import { Request, Response } from "express";
 
 export class DomenMiddleware implements NestMiddleware {
+	private readonly allowedDomains = ['localhost', 'gaplashamizmi-api.up.railway.app'];
+	private readonly allowedIp = ['100.64.0.2'];
 
-	private readonly allowedDomains = ['localhost', 'gaplashamizmi-api.up.railway.app']
-	private readonly allowedIp = ['::ffff:100.64.0.2']
 	use(req: Request, res: Response, next: (error?: Error | any) => void) {
-		const host = req.headers.host?.split(':')[0];
-		const ipAdress = req.ip
+		// Host nomini olish va portni kesib tashlash
+		const host = req.headers.host?.split(':')[0] || '';
 
-		console.log(host);
-		console.log(req.ip);
+		// IP manzilni olish (IPv4 formatida)
+		const ipAdress = req.socket.remoteAddress?.replace(/^.*:/, '') || '';
 
-		if (!this.allowedDomains.includes(host) && !this.allowedIp.includes(ipAdress)) {
+		console.log(`Host: ${host}`);
+		console.log(`allowed: ${this.allowedIp} -> thisIp: ${ipAdress}`);
+
+		// Ruxsat berilgan domen yoki IP bo'lmasa, xatolik chiqarish
+		if (!this.allowedDomains.includes(host) || !this.allowedIp.includes(ipAdress)) {
 			throw new ForbiddenException('Access denied');
 		}
 
