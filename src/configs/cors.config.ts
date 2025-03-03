4eac20c9e584339e0ea4ed06188f4104e3174918
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-03 17:38:16 +0500

    5500 ochirib qoyildi

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -1,9 +1,9 @@
 import { INestApplication } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
-
+// "http://127.0.0.1:5500"
 export function corsConfig(app: INestApplication) {
 	app.enableCors({
-		origin: ["http://localhost:5173", "https://gaplashamiz.netlify.app", "http://127.0.0.1:5500"],
+		origin: ["http://localhost:5173", "https://gaplashamiz.netlify.app", ],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 	});
 	app.use(cookieParser())
