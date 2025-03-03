7da4486549498ea27936d9fbe1172f7df33587f9
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-03 17:20:45 +0500

    ip adress tekshiruvi o'chirildi

--- src/common/middlewares/domen.middleware.ts
+++ src/common/middlewares/domen.middleware.ts
@@ -6,20 +6,15 @@ export class DomenMiddleware implements NestMiddleware {
 	private readonly allowedIp = ['100.64.0.2'];
 
 	use(req: Request, res: Response, next: (error?: Error | any) => void) {
-		// Host nomini olish va portni kesib tashlash
 		const host = req.headers.host?.split(':')[0] || '';
 
-		// IP manzilni olish (IPv4 formatida)
 		const ipAdress = req.socket.remoteAddress?.replace(/^.*:/, '') || '';
 
-		console.log(`Host: ${host}`);
-		console.log(`allowed: ${this.allowedIp} -> thisIp: ${ipAdress}`);
-
-		// Ruxsat berilgan domen yoki IP bo'lmasa, xatolik chiqarish
-		if (!this.allowedDomains.includes(host) || !this.allowedIp.includes(ipAdress)) {
+		if (!this.allowedDomains.includes(host)) {
 			throw new ForbiddenException('Access denied');
 		}
 
 		next();
 	}
 }
+
