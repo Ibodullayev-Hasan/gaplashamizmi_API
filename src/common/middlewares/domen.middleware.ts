ddd1e956998316790e2e0709998b89ba7847d0b0
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-06-22 21:20:07 +0500

    code updating

--- src/common/middlewares/domen.middleware.ts
+++ src/common/middlewares/domen.middleware.ts
@@ -3,13 +3,16 @@ import { Request, Response } from "express";
 
 export class DomenMiddleware implements NestMiddleware {
 	private readonly allowedDomains = ['localhost', 'gaplashamizmi-api.onrender.com', 'ibodullayevhasan.uz'];
-	private readonly allowedIp = ['100.64.0.2'];
+	private readonly allowedIp = ['68.183.181.222'];
 
 	use(req: Request, res: Response, next: (error?: Error | any) => void) {
 		const host = req.headers.host?.split(':')[0] || '';
 
 		const ipAdress = req.socket.remoteAddress?.replace(/^.*:/, '') || '';
-
+		console.log(ipAdress);
+		
+		console.log(this.allowedIp.includes(ipAdress));
+		
 		if (!this.allowedDomains.includes(host)) {
 			throw new ForbiddenException('Access denied');
 		}
