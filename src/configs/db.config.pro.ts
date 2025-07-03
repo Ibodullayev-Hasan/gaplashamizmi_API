221ed45b98ac8bcfe6afdd9f96754e4e8ce585d4
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-07-03 22:33:29 +0500

    fixed db connection error

--- src/configs/db.config.pro.ts
+++ src/configs/db.config.pro.ts
@@ -1,10 +1,12 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import * as path from 'path';
 
+
 export default (): PostgresConnectionOptions => ({
 
   url: process.env.DATABASE_URL as string,
+  migrations:[],
   type: 'postgres',
   entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
-  synchronize: true
+  synchronize: false
 });
